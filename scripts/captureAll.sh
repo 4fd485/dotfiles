831f1163d163cfacee73dc90142ad5ce7c3e5573
gruen <158203519+gruensys@users.noreply.github.com>
2024-06-11 21:43:36 +0000

    Update captureAll.sh

--- scripts/captureAll.sh
+++ scripts/captureAll.sh
@@ -4,7 +4,7 @@
 grim "$HOME/Pictures/Screenshots/$(date +'%s_grim.png')"
 
 # Play sound
-play /home/fj/Music/assets/sh.mp3 &> /dev/null
+play "$HOME/.config/assets/screenshot.mp3" &> /dev/null
 
 # Display a notification
 dunstify -u critical "📸 Screenshot taken!" --icon=dialog-information
